05b7f720064a12e36b3cdb4ac8de7eb0a8d08c4e
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-07 22:26:53 +0100

    revert: undo from imports

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,14 +4,14 @@
 password encryption module
 
 '''
-from bcrypt import gensalt, hashpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     '''hashes a password with bcrypt'''
-    return hashpw(password.encode(), gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     '''checks if password is valid'''
-    return hashpw(password.encode(), hashed_password) == hashed_password
+    return bcrypt.hashpw(password.encode(), hashed_password) == hashed_password
