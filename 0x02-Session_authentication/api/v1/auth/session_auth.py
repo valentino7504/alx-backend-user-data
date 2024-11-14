75947b679d89afd5ba567a3b299845f613ccb141
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-14 05:45:38 +0100

    feat: session_auth base

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+
+A module to implement session auth for ALX tasks
+
+'''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''The session authentication class'''
+    pass
