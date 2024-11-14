ad5fd5465d034e9b47c61f57fa2548056c286f74
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-14 09:33:07 +0100

    feat: get user ID from session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ A module to implement session auth for ALX tasks
 import uuid
 
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -35,3 +36,9 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''returns current user'''
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        return User.get(user_id)
