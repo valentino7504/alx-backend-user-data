17413be9af04e793b35cfb557c41eca7521e87e3
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-14 08:50:15 +0100

    feat: get user id using session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     '''The session authentication class'''
 
-    user_id_by_session_id = {}
+    user_id_by_session_id: dict = {}
 
     def __init__(self):
         '''dunder init method'''
@@ -24,6 +24,14 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        session_id = uuid.uuid4()
-        self.user_id_by_session_id[str(session_id)] = user_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''get user id based on session id'''
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
