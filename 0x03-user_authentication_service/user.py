51f0b9cddda51863b259ac6e697e79990ce2ba4e
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-18 21:08:35 +0100

    feat: create User model with sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+
+This is the module for defining the user model
+
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    '''the user model'''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
