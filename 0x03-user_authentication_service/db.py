8c13eda4881775376d19370adb65f5a0befc700e
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 13:06:26 +0100

    fix: remove unnecessary dec base import

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,6 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
