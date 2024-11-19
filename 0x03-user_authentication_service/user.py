ea50b9ec470d9641c1fa40f0603577b120b59943
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 10:43:52 +0100

    fix: add length to sqlalchemy string

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,8 +13,8 @@ Base = declarative_base()
 class User(Base):
     '''the user model'''
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
