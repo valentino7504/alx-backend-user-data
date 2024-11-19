1659e9b41f5790a608f620546e0d5869553332cb
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 19:12:36 +0100

    feat: add login feature

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,7 +4,7 @@
 The flask app
 
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 from auth import Auth
 
@@ -28,6 +28,19 @@ def register_user():
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
     return jsonify({'email': email, 'message': 'user created'})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''logs in a user'''
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+    if not AUTH.valid_login(email, pwd):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == '__main__':
