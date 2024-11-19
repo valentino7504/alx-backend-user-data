e7ccd805117c9ae34722a0d7266e3e9e34d29f4c
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 18:35:00 +0100

    fix: decode in valid_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,4 +40,4 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return False
-        return bcrypt.checkpw(password, user.hashed_password)
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
