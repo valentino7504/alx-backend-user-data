7818c616d1878d0e5c2f526f9165c0092c0279c8
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 17:57:55 +0100

    feat(app): add POST mechanism

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,8 +4,11 @@
 The flask app
 
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
+from auth import Auth
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +16,18 @@ app = Flask(__name__)
 def welcome_route():
     '''welcome to the app'''
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    '''registers users'''
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+    try:
+        AUTH.register_user(email, pwd)
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
+    return jsonify({'email': email, 'message': 'user created'})
 
 
 if __name__ == '__main__':
