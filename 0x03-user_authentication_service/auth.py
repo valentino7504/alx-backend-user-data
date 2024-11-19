4df1302a5e105c9e3a45337dc099f2bf65fcf437
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 17:15:29 +0100

    fix: remove brackets in class definition

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,7 +18,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-class Auth():
+class Auth:
     '''Auth class to interact with the authentication database.
     '''
 
