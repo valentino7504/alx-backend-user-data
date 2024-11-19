e26f0cb24e1fed53c00c5de8fa01c7ad2515c845
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 19:20:35 +0100

    fix: fetch user with session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,7 +66,7 @@ class Auth:
         if session_id is None:
             return None
         try:
-            user = self._db.find_user_by()
+            user = self._db.find_user_by(session_id=session_id)
         except (NoResultFound, InvalidRequestError):
             return None
         else:
