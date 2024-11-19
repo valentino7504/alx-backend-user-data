213cb2fdccc5bbe480b0df5133b017ab27a4fa90
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 20:46:27 +0100

    feat: get reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         '''destroys a session from user instance'''
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''returns reset token'''
+        try:
+            user = self._db.find_user_by(email=email)
+        except (NoResultFound, InvalidRequestError):
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
