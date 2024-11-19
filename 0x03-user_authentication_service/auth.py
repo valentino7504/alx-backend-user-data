1eb3350f54453c25db2bc70128fb24f4adb920c4
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 19:04:34 +0100

    feat: add generate_uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,9 +45,13 @@ class Auth:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
 
+    def _generate_uuid(self) -> str:
+        '''returns string of uuid'''
+        return str(uuid.uuid4())
+
     def create_session(self, email: str) -> str:
         '''creates a session'''
-        new_session = str(uuid.uuid4())
+        new_session = self._generate_uuid()
         try:
             user = self._db.find_user_by(email=email)
         except (NoResultFound, InvalidRequestError):
