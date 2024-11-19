990a2137b60b4535728fd4b046e45dd126afc6b9
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 18:31:14 +0100

    refactor: register

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,16 +4,14 @@
 authentication module
 
 '''
-
 import bcrypt
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
 from user import User
 
 
-def _hash_password(password: str) -> bytes:
+def _hash_password(password: str) -> str:
     '''hashes password'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
@@ -29,13 +27,12 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         '''registers a user'''
         try:
-            find_user = self._db.find_user_by(email=email)
-            if find_user:
-                raise ValueError("User {} already exists".format(email))
-        except (NoResultFound, InvalidRequestError):
-            hashed = _hash_password(password).decode('utf-8')
-            user = self._db.add_user(email, hashed)
-            return user
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed = _hash_password(password)
+            return self._db.add_user(email, hashed)
+        else:
+            raise ValueError('User {} already exists'.format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
         '''validates a login'''
