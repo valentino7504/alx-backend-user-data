bc0e1eaa8afbada53c0d88f8cba508bb76b7d167
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 16:56:41 +0100

    revert: type annotation error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,7 +13,7 @@ from db import DB
 from user import User
 
 
-def _hash_password(password: str) -> str:
+def _hash_password(password: str) -> bytes:
     '''hashes password'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
