eec8c8b81ba9d330a123cb4027ee13b22ffc21ab
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 13:05:24 +0100

    fix: raise InvalidRequestError and remove select

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
