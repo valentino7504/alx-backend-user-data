86386fc165ef84cd7d6ceec0f2f6b977815c1322
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-19 19:19:06 +0100

    fix: implement none check for session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,6 +63,8 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         '''gets user using the session id'''
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by()
         except (NoResultFound, InvalidRequestError):
