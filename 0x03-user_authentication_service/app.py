6602ebf4f227e4b66118d8eb3f5005984bd62231
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-20 11:51:02 +0100

    fix: change route of update password to reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,7 +79,7 @@ def get_reset_password_token():
     return jsonify({'email': email, 'reset_token': reset_token}), 200
 
 
-@app.route('/update_password', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     '''update password route'''
     email = request.form.get('email')
