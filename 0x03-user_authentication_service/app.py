2e4f6f90a6eca0c34bee3120bf0d78e43903c4c4
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-20 11:48:52 +0100

    feat: add update password to app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,18 @@ def get_reset_password_token():
     return jsonify({'email': email, 'reset_token': reset_token}), 200
 
 
+@app.route('/update_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    '''update password route'''
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_pwd = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_pwd)
+    except ValueError:
+        return abort(403)
+    return jsonify({'email': email, 'message': 'Password updated'})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
