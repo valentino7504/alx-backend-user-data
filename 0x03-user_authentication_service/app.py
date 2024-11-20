d5650da07719a48f6877a8297b9dfd78248c3ce4
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-20 11:32:54 +0100

    feat: get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,5 +68,16 @@ def profile():
     return jsonify({'email': user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    '''POST for reset password token'''
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        return abort(403)
+    return jsonify({'email': email, 'reset_token': reset_token}), 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
